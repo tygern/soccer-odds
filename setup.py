ed6adeb16ebc154db23e5e879de8bea303f279c9
Tyson Gern <tyson@gern.org>
2019-01-04 21:21:27 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Soccer Odds',
+      version='0.0.1',
+      description='Soccer Odds',
+      packages=['soccerodds.fixtures', 'soccerodds.predictions'],
+      )
