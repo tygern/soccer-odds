637fa16e7580242f0db1c1b5823aa083c23a08a1
Tyson Gern <tyson@gern.org>
2019-01-04 21:42:12 -0700

    Use abstract base class

--- soccerodds/predictions/prediction_engine.py
+++ soccerodds/predictions/prediction_engine.py
@@ -1,6 +1,9 @@
+from abc import ABC, abstractmethod
+
 from soccerodds.fixtures.fixture import Fixture, Probabilities
 
 
-class PredictionEngine(object):
+class PredictionEngine(ABC):
+    @abstractmethod
     def predict(self, fixture: Fixture) -> Probabilities:
         pass
